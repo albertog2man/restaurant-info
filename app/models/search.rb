fdf4b44d4b989e86ab4b2edb289e05d148cad65b
Albert Galvez <albertomgalvez@gmail.com>
2015-12-07 05:10:12 -0500

    debugging heroku:

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search
 		options.each do |v,k|
 			params += "#{v}=#{k}&"
 		end
-		response = HTTParty.get("https://api.foursquare.com/v2/venues/search?intent=#{intent}&#{params}client_id=DGYOAJXYGFIC5Y44Z5JA5HEU10CXPDF0UKQEZQA4JMPIOVDZ&client_secret=SD1WOKW14QOLFF1PWHJ2HGE3DDZS1OW40HRULDCOXAGXDFGE&v=20140806")
+		response = HTTParty.get(URI.encode("https://api.foursquare.com/v2/venues/search?intent=#{intent}&#{params}client_id=DGYOAJXYGFIC5Y44Z5JA5HEU10CXPDF0UKQEZQA4JMPIOVDZ&client_secret=SD1WOKW14QOLFF1PWHJ2HGE3DDZS1OW40HRULDCOXAGXDFGE&v=20140806"))
 		puts response.body
 		JSON.parse(response.body)
 	end
